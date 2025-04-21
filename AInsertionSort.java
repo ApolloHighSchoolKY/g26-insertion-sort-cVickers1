9dd60534da528a1e0311cb5b7e0f0ebde392d048
cVickers1 <cameron.vickers@stu.daviess.kyschools.us>
2025-04-21 12:37:39 +0000

    zawg

--- AInsertionSort.java
+++ AInsertionSort.java
@@ -4,6 +4,14 @@ public class AInsertionSort
     public static void main(String[] args)
     {
     	Integer myNumbers[] = {0, 15, 6, 8, 2, 37};
-
+        int shlawg;
+        for (int i = 1; i < myNumbers.length; i++)
+        {
+            for(int l = i-1; l>0; l--)
+            {
+                if(myNumbers[l]<myNumbers[l-1])
+                shlawg = myNumbers[]
+            }
+        }
     }
 }
--- ALInsertionSort.java
+++ ALInsertionSort.java
@@ -10,17 +10,22 @@ public class ALInsertionSort
     	boolean inserted=false;
 
 		//Add the first number to the array list
-
+		sorted.add(myNumbers[0]);
 		//Loop once for all of the remaining numbers in the unsorted list
-
+		for(int i = 1; i < myNumbers.length; i++)
+		{
 			//Check with each of the numbers in the sorted list
-
+			for(int zawg = 0; zawg < myNumbers.length; zawg++)
+			{
 				//If this number is less than one in the sorted list,
 				//insert it there
+				
 
+
+			}
 			//If it was not inserted, stick it on the end.
 
-
+		}
 		//End Loop for unsorted list
 
 		System.out.println(sorted);
